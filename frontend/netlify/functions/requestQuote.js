80069086a7ef561ea212e0cf7316263c1fba1e55
Cameron McBroom <cam.mcbroom@gmail.com>
2022-03-30 21:48:13 +1100

    updated quote request function for better logging and some small changes

--- frontend/netlify/functions/requestQuote.js
+++ frontend/netlify/functions/requestQuote.js
@@ -7,19 +7,20 @@ exports.handler = async function (event, context) {
     const {body} = context;
 
     try {
-        const response = await axios.post(`/quote-form-submission`, {data: body}, {
+        const response = await axios.post(`${process.env.API_BASE_URL}/quote-form-submission`, {data: body}, {
             headers: {
                 Authorization:
                     `Bearer ${process.env.STRAPI_KEY}`
             }
         });
-
+        console.log("Success Response: ", response)
         return {
             statusCode: 200,
             body: JSON.stringify(response.data)
         }
     } catch (e)
         {
+            console.log("Failed with error: ", e)
             return {
                 statusCode: 404,
                 body: e.toString(),
